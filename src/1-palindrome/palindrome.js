7fd4f41e27de7bdc02fda0f753bd7566892f562e
Guilherme <guilhermevargasrodrigues8@gmail.com>
2024-11-26 20:06:33 -0300

    changing

--- src/1-palindrome/palindrome.js
+++ src/1-palindrome/palindrome.js
@@ -1,10 +1,10 @@
-function isPalindrome(str) {
-    if (!str) {
+function isPalindrome(string) {
+    if (!string) {
         return false;  // Verifica se a string é vazia ou nula
     }
 
     // Remove caracteres que não são letras ou números e converte para minúsculas
-    const cleanedString = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const cleanedString = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
     // Compara a string limpa com a sua versão invertida
     return cleanedString === cleanedString.split('').reverse().join('');
